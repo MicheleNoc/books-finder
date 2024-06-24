d79b654ea5989c57ef84480dfe1bf21904d59974
Michele Nocchi <michelenocchi@MacBook-Pro.local>
2024-06-24 22:35:05 +0200

    modificato index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import path from "path";
 
 const app = express();
 const port = process.env.PORT ||    3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
