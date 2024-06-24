fe37c76dd1963406d4486858d9067737007c0677
Michele Nocchi <michelenocchi@MacBook-Pro.local>
2024-06-24 23:04:32 +0200

    modificato index.js

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 
+
 const __dirname = path.resolve();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // Middleware per gestire dati JSON e URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Imposta EJS come motore di rendering
 app.set('view engine', 'ejs');
 // Imposta la directory delle viste
